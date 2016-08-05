d1701d6ab838624febc872230f66efedcc6f0a4a
Kevin Mathews <kpam1992@gmail.com>
2016-08-05 14:18:14 -0700

    finish subs

--- RedditOnRails/app/controllers/subs_controller.rb
+++ RedditOnRails/app/controllers/subs_controller.rb
@@ -5,9 +5,15 @@ class SubsController < ApplicationController
     render :new
   end
 
+  def index
+    @subs = Sub.all
+    render :index
+  end
+
   def create
     @sub = Sub.new(sub_params)
     if @sub.save
+      # fail
       redirect_to sub_url(@sub)
     else
       flash.now[:errors] = @sub.errors.full_messages
@@ -23,13 +29,23 @@ class SubsController < ApplicationController
     render :edit
   end
 
+  def update
+    @sub = Sub.find_by(sub_params)
+    if @sub.update
+      redirect_to sub_url(@sub)
+    else
+      redirect_to edit_sub_url(@sub)
+    end
+  end
+
   def show
+    @sub = Sub.find_by(id: params[:id])
     render :show
   end
 
   private
   def sub_params
-    params.require(:sub).permit(:title, :descrption, :moderator_id)
+    params.require(:sub).permit(:title, :description, :moderator_id)
   end
 
 end
