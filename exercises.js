7c8c5604d2b2ec801cabfbfe12da92545bcebe75
Kevin Mathews <kpam1992@gmail.com>
2016-08-15 12:02:28 -0700

    completed argument exercises

--- exercises.js
+++ exercises.js
@@ -8,12 +8,9 @@ function sum() {
 }
 
 
-Function.prototype.myBind = function(obj) {
-  let args = Array.from(arguments);
-  args.shift();
+Function.prototype.myBind = function(obj, ...args) {
   let thisFunc = this;
-  let result = function () {
-    let moreArgs = Array.from(arguments);
+  let result = function (...moreArgs) {
     return thisFunc.apply(obj, args.concat(moreArgs));
   };
   return result;
@@ -34,4 +31,63 @@ class Cat {
 const markov = new Cat("Markov");
 const breakfast = new Cat("Breakfast");
 const notMarkovSays = markov.says.myBind(breakfast);
-console.log(notMarkovSays("meow", "me"));
+// console.log(notMarkovSays("meow", "me"));
+
+
+function curriedSum(numArgs) {
+  let numbers = [];
+
+  let _curriedSum = function(num) {
+    numbers.push(num);
+    if (numbers.length === numArgs) {
+      return sum(...numbers);
+    }
+    else {
+      return _curriedSum;
+    }
+  };
+  return _curriedSum;
+}
+
+// const thing = curriedSum(4);
+// console.log(thing(5)(30)(20)(1));
+
+
+Function.prototype.curry = function(numArgs) {
+  let args = [];
+  let func = this;
+  function _curry(argument) {
+    args.push(argument);
+    if (args.length === numArgs) {
+      return func(...args);
+    }
+    else {
+      return _curry;
+    }
+  }
+  return _curry;
+};
+
+
+function sumThree(num1, num2, num3) {
+  return num1 + num2 + num3;
+}
+
+sumThree(4, 20, 6); // == 30
+
+// you'll write `Function#curry`!
+let f1 = sumThree.curry(3); // tells `f1` to wait until 3 arguments are given before running `sumThree`
+f1 = f1(4); // [Function]
+f1 = f1(20); // [Function]
+f1 = f1(6); // = 30
+console.log(f1);
+// or more briefly:
+
+console.log(sumThree.curry(3)(4)(20)(6)); // == 30
+// console.log();
+
+
+
+
+
+//
