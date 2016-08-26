1bddd51bd4b8d1975f7bf95e337ad20f0c53f096
Kevin Mathews <kpam1992@gmail.com>
2016-08-25 17:57:49 -0700

    Got the first route working

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,11 +1,14 @@
 import {Provider} from 'react-redux';
 import React from 'react';
 import PokemonIndexContainer from "./pokemon/pokemon_index_container.jsx";
+import { Router, Route, hashHistory } from 'react-router';
 
 const Root = (props) => {
   return (
     <Provider store={props.store}>
-      <PokemonIndexContainer />
+      <Router history={hashHistory}>
+        <Route path='/' component={PokemonIndexContainer}></Route>
+      </Router>
     </Provider>
   );
 };
