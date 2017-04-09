3d3542db99d467f44fe1683154538d3c47bd59d3
kpam92 <kpam1992@gmail.com>
2017-04-08 17:31:27 -0700

    a03

--- a03-prep-master/assoc-skeleton/app/models/board.rb
+++ a03-prep-master/assoc-skeleton/app/models/board.rb
@@ -1,10 +1,12 @@
 class Board < ActiveRecord::Base
 
-  has_many :board_memberships,
+  has_many :memberships,
   primary_key: :id,
-  foreign_key: :board_id
+  foreign_key: :board_id,
+  class_name: "BoardMembership"
 
   belongs_to :company,
   primary_key: :id,
-  foreign_key: :company_id
+  foreign_key: :company_id,
+  class_name: "Company"
 end
--- a03-prep-master/assoc-skeleton/app/models/company.rb
+++ a03-prep-master/assoc-skeleton/app/models/company.rb
@@ -1,2 +1,7 @@
 class Company < ActiveRecord::Base
+
+  has_many :boards,
+  primary_key: :id,
+  foreign_key: :company_id,
+  class_name: "Board"
 end
