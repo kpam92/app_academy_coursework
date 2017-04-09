8bc12d8cecdfb87ddfa47c53890b06f2c2c1fedf
kpam92 <kpam1992@gmail.com>
2017-04-08 19:12:34 -0700

    a03 associations done

--- a03-prep-master/assoc-skeleton/app/models/watch_list.rb
+++ a03-prep-master/assoc-skeleton/app/models/watch_list.rb
@@ -9,4 +9,9 @@ class WatchList < ActiveRecord::Base
   primary_key: :id,
   foreign_key: :watch_list_id,
   class_name: "WatchListItem"
+
+  belongs_to :user,
+  primary_key: :id,
+  foreign_key: :user_id,
+  class_name: "User"
 end
