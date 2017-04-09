6a85b1124024fcb6ba81fc7841a5e196353f5633
kpam92 <kpam1992@gmail.com>
2017-04-08 17:19:41 -0700

    a03

--- a03-prep-master/assoc-skeleton/app/models/board.rb
+++ a03-prep-master/assoc-skeleton/app/models/board.rb
@@ -1,2 +1,10 @@
 class Board < ActiveRecord::Base
+
+  has_many :board_memberships,
+  primary_key: :id,
+  foreign_key: :board_id
+
+  belongs_to :company,
+  primary_key: :id,
+  foreign_key: :company_id
 end
--- a03-prep-master/assoc-skeleton/app/models/board_membership.rb
+++ a03-prep-master/assoc-skeleton/app/models/board_membership.rb
@@ -1,2 +1,7 @@
 class BoardMembership < ActiveRecord::Base
+
+  belongs_to :board,
+  primary_key: :id,
+  foreign_key: :board_id
+
 end
