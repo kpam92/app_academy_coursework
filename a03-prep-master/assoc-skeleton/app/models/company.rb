03f74a51f0c08b0beb79243cca18f604b1fa489f
kpam92 <kpam1992@gmail.com>
2017-04-08 18:05:02 -0700

    a03

--- a03-prep-master/assoc-skeleton/app/models/company.rb
+++ a03-prep-master/assoc-skeleton/app/models/company.rb
@@ -14,4 +14,17 @@ class Company < ActiveRecord::Base
   primary_key: :id,
   foreign_key: :company_id,
   class_name: "Price"
+
+  has_many :watch_list_items,
+  primary_key: :id,
+  foreign_key: :company_id,
+  class_name: "WatchListItem"
+
+  has_many :watch_lists,
+  through: :watch_list_items,
+  source: :watch_list
+
+
+
+
 end
